d8897d3a48fb6730e49a1e87cd6480dc182c7175
pctechkid <omboyronnel@gmail.com>
2024-06-02 18:52:25 +0800

    fix

--- custom.js
+++ custom.js
@@ -5,8 +5,8 @@ module.exports = async ({ api }) => {
   const axios = require("axios");
   const yandeva = {
     autoRestart: {
-      status: false,
-      time: 30, //40 minutes
+      status: true,
+      time: 10, //40 minutes
       note: "To avoid problems, enable periodic bot restarts",
     },
     acceptPending: {
